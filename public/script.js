9f3aacd91516a23223da398b59960cb6207f5783
Rohit Nair <admin@rohitnair.net>
2014-09-06 02:22:43 +0400

    Simple UI to create a booking

--- public/script.js
+++ public/script.js
@@ -12,4 +12,12 @@ app.controller('BookingController', function($scope, $http){
     d = new Date(time);
     return d.toTimeString().substr(0, 5);
   }
+
+  $scope.createBooking = function(time){
+    if(confirm('Are you sure you want to make a booking for this time?')){
+      $http.post('/api/book/' + time + '/' + parseInt($scope.bookingDuration) + '/' + parseInt($scope.bookingRecurrence)).success(function(){
+        $scope.checkAvailability();
+      });
+    }
+  }
 });
