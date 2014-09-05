c55015c5064346340109ffc443adc342a343b4d8
Rohit Nair <admin@rohitnair.net>
2014-09-05 17:47:59 +0400

    Very basic database setup

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'gyhtest',
-  password : 'gyhtest'
+  password : 'gyhtest',
+  database : 'gyhtest'
 });
 
 connection.connect(function(err){
@@ -13,6 +14,21 @@ connection.connect(function(err){
     return;
   }
   console.log('Successfully connected' + connection.threadId);
+});
+
+// Set up the table if needed
+var tableSetupQuery = "CREATE TABLE IF NOT EXISTS `bookings` ( \
+  id INT NOT NULL AUTO_INCREMENT, \
+  start DATETIME NOT NULL, \
+  end DATETIME NOT NULL, \
+  PRIMARY KEY ( id ) \
+  );";
+
+connection.query(tableSetupQuery, function(err){
+  if(err){
+    console.error('Table setup failed: ' + err.stack);
+    return;
+  }
 });
 
 var app = express();
--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@ Booking Mini App
 ================
 
 This document will serve as a guide to my thoughts while working on this challenge.
+
+Since the challenge required the solution to be in a single Node module, I've decided 
+to include the table setup code as a simple query right at the top. Ordinarily, this
+would be better handled via some sort of migration system.
